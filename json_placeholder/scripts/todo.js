c9d22a4f2acfb7b96f5fe3c4a80620b685a5af2c
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-04 12:07:49 -0500

    error handling

--- json_placeholder/scripts/todo.js
+++ json_placeholder/scripts/todo.js
@@ -1,7 +1,36 @@
 const inputValue = document.querySelector("form");
 const input = inputValue.todoId;
 
+const getData = async (user) => {
+  try {
+    if (!isNaN(user)) {
+      let res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${user}`
+      );
+      res = await res.json();
+      if (res.status === 200 || 304) {
+        return res;
+      } else {
+        throw new Error(res);
+      }
+    } else {
+      throw new Error("Enter a Number for id");
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
 inputValue.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(input.value);
+
+  const handleAsyncData = async () => {
+    const data = await getData(input.value);
+    console.log(data);
+  };
+  try {
+    handleAsyncData();
+  } catch (error) {
+    console.log(error);
+  }
 });
